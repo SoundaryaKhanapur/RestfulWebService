3922d48f06a9a86de096738cb32625b7dd0c41b6
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2021-03-28 12:06:46 +0530

    GETMapping

--- src/main/java/com/restful/webservice/restfulwebservice/HelloController.java
+++ src/main/java/com/restful/webservice/restfulwebservice/HelloController.java
@@ -1,5 +1,6 @@
 package com.restful.webservice.restfulwebservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
-	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping(path = "/helloworld")
 	public String helloWorld() {
 		return "Hello World!";
 	}
